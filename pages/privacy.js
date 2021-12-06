adda132ba0a61468be348485982242fd18b8452a
TarranKartikTellus <tarrankartiktellus@gmail.com>
2021-12-06 16:54:20 +0530

    privacy and terms page begins

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,1 +1,7 @@
 //privacy policy
+
+export default function Privacy(){
+return (
+  <div  className="text-gray-100 text-3xl">Privacy Policy</div>
+ );
+}
--- pages/terms.js
+++ pages/terms.js
@@ -1,1 +1,7 @@
 //Terms of service
+
+export default function Terms(){
+return (
+  <div className="text-gray-100 text-3xl">Terms of service</div>
+ );
+}
