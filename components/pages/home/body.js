eb3c3e5ba1550d03c5c8d5c8cb4184ddd82d3162
TarranKartikTellus <tarrankartiktellus@gmail.com>
2021-12-25 11:01:59 +0530

    pix fix

--- components/pages/home/body.js
+++ components/pages/home/body.js
@@ -9,7 +9,7 @@ export default function Body({data}){
    <div className=" w-screen  bg-transparent flex justify-center   ">  
     <div className="flex flex-col   items-center    w-10/12 md:w-3/6 ">
       
-      <div className="flex flex-col items-center text-slate-100 mt-12">
+      <div className="flex flex-col items-center text-slate-100 mt-24 md:mt-12">
         <SlideVr initialOpacity={0} animateOpacity={1} exitOpacity={1} animateDelay={0.4} animateDuration={0.5} initialLocation={40} animateLocation={0} exitLocation={0}>
         <Scale initialScale={0.8} animateScale={1} exitScale={0.8} animateDelay={0.5} animateDuration={1} >
         <div className="relative w-16 h-16 md:w-32 md:h-40 scale-110 animate-wiggle"><img className="animate-infinite_glow scale-95 hover:scale-105 hover:opacity-100 transition-transform duration-300" src="/svg/myLogo.svg" /></div>
